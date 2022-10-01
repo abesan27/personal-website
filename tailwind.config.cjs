cae76b15461a0a2dd84820bf263a69f1bea11c0d
Albert Stan <albertstan30@gmail.com>
2022-10-02 00:13:17 +0200

    sizing and tech section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    screens: {
+      sm: "600px",
+    },
     container: {
       center: true,
       padding: "1rem",
       screens: {
-        "2xl": "1024px",
+        "2xl": "600px",
       },
     },
     extend: {},
